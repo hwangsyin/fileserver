b627f905b9db6b500e52a29ccda4472337086be6
hwangsyin <hwangsyin@gmail.com>
2016-05-31 00:46:38 +0800

    config

--- cmd/fileserver/main.go
+++ cmd/fileserver/main.go
@@ -4,11 +4,18 @@ import (
 	"log"
 	"net/http"
 	"handler"
+	"config"
 )
 
 func main() {
+	err := config.Parse()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	http.HandleFunc("/files", handler.HandleFile)
-	err := http.ListenAndServe(":8080", nil)
+	
+	err = http.ListenAndServe(config.Configs["port"], nil)
 	if err != nil {
 		log.Fatal(err)
 	}
--- config/config.go
+++ config/config.go
@@ -1,5 +1,58 @@
 package config
 
 import (
-	
+	"os"
+	"path/filepath"
+	"errors"
+	"io/ioutil"
+	"encoding/xml"
 )
+
+var (
+	Configs = make(map[string]string, 16)
+)
+
+type property struct {
+	XMLName xml.Name `xml:"property"`
+	Name string `xml:"name"`
+	Value string `xml:"value"`
+}
+
+type configuration struct {
+	XMLName xml.Name `xml:"configuration"`
+	Properties []property `xml:"property"`
+}
+
+func Parse() error {
+	home, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return err
+	}
+	
+	configFileName := filepath.Join(home, "conf", "server.xml")
+	fileInfo, err := os.Stat(configFileName)
+	if os.IsNotExist(err) {
+		return errors.New(configFileName + " not exists")
+	}
+	
+	if !fileInfo.Mode().IsRegular() {
+		return errors.New(configFileName + " is not regular file")
+	}
+	
+	content, err := ioutil.ReadFile(configFileName)
+	if err != nil {
+		return err
+	}
+	
+	var result configuration
+	err = xml.Unmarshal(content, &result)
+	if err != nil {
+		return err
+	}
+	
+	for _, p := range result.Properties {
+		Configs[p.Name] = p.Value
+	}
+	
+	return nil
+}
